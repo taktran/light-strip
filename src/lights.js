5f8962ca3f888108c2219347fa9fdb96195df21b
Tak Tran <contact@tutaktran.com>
2015-12-01 22:22:06 +0000

    Add ability to change the colour in the repl

--- src/lights.js
+++ src/lights.js
@@ -6,12 +6,24 @@ var board = new five.Board();
 board.on("ready", function () {
   var strip = new pixel.Strip({
     data: 6,
-    length: 10,
+    length: 30, // Doesn't seem to do anything
     board: this,
     controller: "FIRMATA",
   });
 
+  function showColor(color) {
+    strip.color(color);
+    strip.show();
+  }
+
+  board.repl.inject({
+    strip: strip,
+    showColor: showColor
+  });
+
   strip.on("ready", function () {
-    
+    console.log("Pixels ready\n");
+
+    showColor('#111111');
   });
 });
