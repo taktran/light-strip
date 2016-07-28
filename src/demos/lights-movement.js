51a82f25a3a0abb4e87351b8d167ee5940988dc4
Tak Tran <contact@tutaktran.com>
2016-07-28 23:11:47 +0100

    es6-ify, and add play function to start animation

--- src/demos/lights-movement.js
+++ src/demos/lights-movement.js
@@ -14,59 +14,32 @@
  *
  * Based on https://github.com/ajfisher/node-pixel/blob/master/examples/johnnyfive-i2c.js
  */
-var pixel = require('node-pixel');
-var five = require('johnny-five');
-var Tweenable = require('shifty');
-var _ = require('lodash');
+const pixel = require('node-pixel');
+const five = require('johnny-five');
+const Tweenable = require('shifty');
+const _ = require('lodash');
 
-var board = new five.Board();
+const board = new five.Board();
 
 board.on('ready', function () {
-  var options = {
+  const options = {
     pin: 6,
     length: 10,
     board: this,
     controller: 'FIRMATA'
   };
-  var strip = new pixel.Strip(options);
-  var tweenable = new Tweenable();
+  const strip = new pixel.Strip(options);
+  const tweenable = new Tweenable();
   function off () {
     strip.off();
     strip.show();
   }
-
-  var currentIndex;
-  var color = '#f3f3f3';
-  function showColor () {
-    if (!currentIndex) { return; }
-    strip.clear();
-
-    var pixel = strip.pixel(currentIndex);
-    if (pixel) {
-      pixel.color(color);
-    } else {
-      console.log('No pixel found', strip, currentIndex);
-    }
-
-    strip.show();
-  }
-  var fps = 20;
-  var throttleTime = 1000 / fps;
-  var throttledShowColor = _.throttle(showColor, throttleTime);
-
-  board.repl.inject({
-    strip: strip,
-    showColor: showColor,
-    off: off
-  });
-
-  strip.on('ready', function () {
-    console.log('Pixels ready\n');
-    var fromPosition = 0;
-    var toPosition = 10;
-    var easing = 'easeOutQuart';
-    var duration = 5000;
-    var step = function (state) {
+  function play () {
+    const fromPosition = 0;
+    const toPosition = options.length;
+    const easing = 'easeOutBounce';
+    const duration = 5000;
+    const step = function (state) {
       currentIndex = Math.round(state.x);
       console.log('currentIndex', currentIndex);
       throttledShowColor();
@@ -81,5 +54,34 @@ board.on('ready', function () {
     });
 
     strip.show();
+  }
+
+  let currentIndex;
+  let color = '#f3f3f3';
+  function showColor () {
+    if (!currentIndex) { return; }
+    strip.clear();
+
+    var pixel = strip.pixel(currentIndex);
+    if (pixel) {
+      pixel.color(color);
+      strip.show();
+    } else {
+      console.log('No pixel found', strip, currentIndex);
+    }
+  }
+  const fps = 20;
+  const throttleTime = 1000 / fps;
+  const throttledShowColor = _.throttle(showColor, throttleTime);
+
+  board.repl.inject({
+    strip: strip,
+    showColor: showColor,
+    off,
+    play
+  });
+
+  strip.on('ready', function () {
+    console.log('Pixels ready\n');
   });
 });
